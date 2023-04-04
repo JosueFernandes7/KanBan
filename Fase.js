6c6bf4091237931d03a46eab6f95ef62382825d1
Josue Fernandes <josue7mf@hotmail.com>
2023-04-04 16:58:06 -0300

    finalizando classe e implementação

---
+++ Fase.js
@@ -0,0 +1,40 @@
+export default class Fase {
+  constructor(name, localization) {
+    this.name = name;
+    this.localization = localization;
+    this.months = ['Janeiro','Fevereiro','Março','Abril','Maio','Junho','Julho','Agosto','Setembro','Outubro','Novembro','Dezembro']
+  }
+  getLocalization() {
+    return this.localization;
+  }
+  getDataAtual() {
+    let diaAtual = new Date().getDate();
+    let mesAtual = this.months[new Date().getMonth()]
+    return `${diaAtual}, ${mesAtual}`
+  }
+  createTask(title, content, author) {
+    const task = document.createElement('div');
+    task.classList.add('task');
+    task.draggable = true
+
+    const h1 = document.createElement('h1');
+    h1.textContent = title;
+
+    const p = document.createElement('p');
+    p.textContent = content;
+
+    const spanAuthor = document.createElement('span');
+    spanAuthor.textContent = author;
+    spanAuthor.classList.add('taskAuthor');
+
+    const spanDate = document.createElement('span');
+    spanDate.textContent = this.getDataAtual();
+    spanDate.classList.add('taskDate');
+
+    task.append(...[h1,p,spanAuthor,spanDate]);
+    this.insertTask(task);
+  }
+  insertTask(task) {
+    this.localization.append(task);
+  }
+}
--- index.js
+++ index.js
@@ -1,11 +1,52 @@
-const todoDropzone = document.querySelector('.dropzone.todo');
-const todoInProgress = document.querySelector('.dropzone.inProgress');
-const todoDone = document.querySelector('.dropzone.done');
+import Fase from './Fase.js'
+
+// console.log(a.getDataAtual());
+let faseSelecionada = null
+const kanBanTodo = document.querySelector('.dropzone.todo')
+const kanBanInProgress = document.querySelector('.dropzone.inProgress')
+const kanBanDone = document.querySelector('.dropzone.done')
+
+const todo = new Fase('Todo', kanBanTodo)
+const inProgress = new Fase('Todo', kanBanInProgress)
+const done = new Fase('Todo', kanBanDone)
+
+const modalTask = document.querySelector('.modalTask')
+const formulario = document.querySelector('[data-formulario]')
+const enviarTask = document.querySelector('#enviarTask')
 
 // Seleciona todos os botões e Ativa uma função referenciando qual a dropzone ele representa
-const todoBTN = document.querySelectorAll(".createTask > button");
-console.log(todoBTN);
+const todoBTN = document.querySelectorAll('.createTask > button')
+console.log(todoBTN)
+todoBTN.forEach((btn) => btn.addEventListener('click', mostrarModal))
 
-todoBTN.forEach(btn => btn.addEventListener('click', ({target}) => {
-  console.log(target.parentNode.parentNode.children[0]);
-}))
+function mostrarModal({ target }) {
+  modalTask.style.display = 'block'
+  modalTask.addEventListener('click', escondeModal)
+
+  faseSelecionada = target.parentNode.parentNode.children[0]
+  enviarTask.addEventListener('click', criarTask)
+}
+
+function criarTask(event) {
+  event.preventDefault()
+  const titulo = formulario.titulo.value
+  const conteudo = formulario.conteudo.value
+  const autor = formulario.autor.value
+  let formEnviado = false
+  ;[todo, inProgress, done].forEach((e) => {
+    if (e.getLocalization() == faseSelecionada) {
+      e.createTask(titulo, conteudo, autor)
+      formEnviado = true;
+    }
+  })
+  if(formEnviado) modalTask.style.display = 'none'
+
+}
+
+// Remove o Modal e o Evento se clicar fora.
+function escondeModal({ target, currentTarget }) {
+  if (target === currentTarget) {
+    modalTask.style.display = 'none'
+    modalTask.removeEventListener('click', escondeModal)
+  }
+}
